a0644a64e5b7274a135f0ebc218836f6a0be5bea
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 08:36:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
 
+  attr_accessor :discount, :total
+
+  def initialize(discount)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity)
+    @total = @total + (price * quantity)
+  end
+end
